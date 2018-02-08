334a684b03d67b291a4a02e675f36a05c83ce9b7
hvdson <hvdsonmusic@gmail.com>
2018-02-08 21:55:44 +0000

    abandon ship

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
+    table.increments('id');
+    table.string('email');
+    table.string('password');
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('users');
 };
+\HTMLQuoteElement

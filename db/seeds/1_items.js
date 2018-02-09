e011a7f748b8d584af0986f3cb31bd7ebacd9c57
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-09 21:51:08 +0000

    fixed seed files

---
+++ db/seeds/1_items.js
@@ -0,0 +1,39 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('items').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('items').insert({ description: 'Watch Lord of the Rings', category: 'watch', user_id: 1}),
+        knex('items').insert({ description: 'Watch Harry Potter', category: 'watch', user_id: 2}),
+        knex('items').insert({ description: 'Catch up on Game of Thrones', category: 'watch', user_id: 3}),
+        knex('items').insert({ description: 'Watch the new Thor', category: 'watch', user_id: 1}),
+        knex('items').insert({ description: 'Checkout the Office', category: 'watch', user_id: 1}),
+        knex('items').insert({ description: 'Watch Better Call Saul', category: 'watch', user_id: 2}),
+        knex('items').insert({ description: 'See the Room', category: 'watch', user_id: 1}),
+        knex('items').insert({ description: 'Watch Lord of the Rings', category: 'watch', user_id: 3}),
+        knex('items').insert({ description: 'Read the Girl with the Dragon Tattoo', category: 'read', user_id: 1 }),
+        knex('items').insert({ description: 'Scan the article on Trump', category: 'read', user_id: 2 }),
+        knex('items').insert({ description: 'Read the new Stephen King novel', category: 'read', user_id: 3 }),
+        knex('items').insert({ description: 'Read the latest Archie comic', category: 'read', user_id: 1 }),
+        knex('items').insert({ description: 'Look through the Vancouver Sun', category: 'read', user_id: 1 }),
+        knex('items').insert({ description: 'Read chapter three of the Psych textbook', category: 'read', user_id: 2 }),
+        knex('items').insert({ description: 'Read a good jokebook', category: 'read', user_id: 1 }),
+        knex('items').insert({ description: 'Read Lord of the Rings', category: 'read', user_id: 3 }),
+        knex('items').insert({ description: 'Eat eggs benny', category: 'eat', user_id: 1 }),
+        knex('items').insert({ description: 'Get brunch with Johnny', category: 'eat', user_id: 2 }),
+        knex('items').insert({ description: 'Try the new sushi place next door', category: 'eat', user_id: 3 }),
+        knex('items').insert({ description: 'Go for burgers', category: 'eat', user_id: 1 }),
+        knex('items').insert({ description: 'Make a salad for lunch', category: 'eat', user_id: 1 }),
+        knex('items').insert({ description: 'Meal prep for next week', category: 'eat', user_id: 2 }),
+        knex('items').insert({ description: 'Meet friends for lunch', category: 'eat', user_id: 1 }),
+        knex('items').insert({ description: 'Eat all of the hot dogs', category: 'eat', user_id: 3 }),
+        knex('items').insert({ description: 'get a new phone case', category: 'buy', user_id: 2 }),
+        knex('items').insert({ description: 'immersion blender', category: 'buy', user_id: 3 }),
+        knex('items').insert({ description: 'buy harry potter DVD collection', category: 'buy', user_id: 1 }),
+        knex('items').insert({ description: 'plant lightbulb', category: 'buy', user_id: 1 }),
+        knex('items').insert({ description: 'get 6m usb lightning cable', category: 'buy', user_id: 2 })
+      ]);
+    });
+};

3121a2058eccb48cf0b5e254536c49a7c50c1930
Laura De Witt <lauramdewitt@gmail.com>
2018-02-10 21:37:42 +0000

    global variables, midway conversion to sass
    defined global font family and colour variables, gave modals id modal and gave clickables consistent colouring. Spaced nav bar elements across screen width. still need to align nav buttons and align heights across navbar elements.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
     url: "/api/items"
   }).done((items) => {
     for(item of items) {
-      const toDoItem = $('<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="myModal">').addClass('toDoItem');
+      const toDoItem = $('<button type="button" class="btn btn-info btn-lg" id="modal" data-toggle="modal" data-target="myModal">').addClass('toDoItem');
       toDoItem.text(item.description).appendTo($(`#${item.category}`));
       // console.log(item);
     }
--- public/scripts/appendElements.js
+++ public/scripts/appendElements.js
@@ -9,28 +9,28 @@ $(document).ready(function () {
     // get the user input from the form
     const taskText = $(this).find('[name="text"]').val();
     // const newTask = $("<div>").text(taskText);
-    const newTask = $('<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">').text(taskText);
+    const newTask = $('<button type="button" class="btn btn-info btn-lg" data-toggle="modal" id="modal" data-target="#myModal">').text(taskText);
     newTask.addClass("toDoItem");
 
     // fixme: wait for server response before appending new task category to the category
 
     // todo: categorize the input text using bayes-classifier
-    
-    //  todo: 
+
+    //  todo:
 
     // todo:
-    
+
     $.post('/homepage', {category: taskText}).done((response) => {
       console.log("got a response:", JSON.stringify(response));
       // FIXME: need to categorize before appending
-      
+
       // server side post categorize works but we need to figure out how to refresh the page on 'add task' to show new task on page
       // const cat = categorize(newTask);
       $(`#${response.category}`).append(newTask);
-        
+
     });
 
-    
+
 
   });
 });

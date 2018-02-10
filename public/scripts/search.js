9614e2bdb3836e0977cdbccd1dc63104d74dc7e3
hvdson <hvdsonmusic@gmail.com>
2018-02-10 22:37:11 +0000

    removed typeof console.log from search.js

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -2,5 +2,5 @@ var google = require('google-query');
 
 google.search("watch lotr imdb", 1, function (url_list) {
     var url_str = url_list.slice(0, 3).join('\n');
-    console.log(typeof(url_str));
+    console.log(url_str);
 });

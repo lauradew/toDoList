bc0edbc93abff4da754e99b4bb44c744cab3f620
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-10 21:56:59 +0000

    played witth google search api

---
+++ public/scripts/search.js
@@ -0,0 +1,6 @@
+var google = require('google-query');
+
+google.search("watch lotr imdb", 1, function (url_list) {
+    var url_str = url_list.slice(0, 3).join('\n');
+    console.log(url_str);
+});

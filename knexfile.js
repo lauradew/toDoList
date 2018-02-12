4a600a3a2f11c4a41c2a6ded0931b18c097f5ab3
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-12 04:12:04 +0000

    heroku

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://deekdvaxxnoykt:af1443e9b400cf73755cea02c9dc018b3ceb5f48b99ec2dc36acb9baf24b0afa@ec2-23-21-229-48.compute-1.amazonaws.com:5432/d5lup1ib1lbfss?ssl=true',
     pool: {
       min: 2,
       max: 10
